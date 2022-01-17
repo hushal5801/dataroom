10bc534a3eb2ec84ab2c857a75a7deffbb631f84
hushal5801 <97868064+hushal5801@users.noreply.github.com>
2022-01-17 11:31:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function login() {
+    window.alert("WORKING@!")
+    var userEmail = document.getElementById("email_field").Value;
+    var userpass = document.getElementById("password_field").Value;
+    window.alert(userEmail + "  " + userpass);
+}
